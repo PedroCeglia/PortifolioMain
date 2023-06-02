fe3163fc10478c82118f28ab0f2cfde5340b3a77
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-06-02 16:24:41 -0300

    Create MenuHeader Component

---
+++ components/EstruturaPagina/Header/MenuHeader/index.jsx
@@ -0,0 +1,43 @@
+'use client'
+import { useState } from "react"
+import {
+    MenuHeaderContainer,
+    MenuToggle, MenuToggleLabel, MenuToggleLabelSpan,
+    MenuBox, MenuBoxItem
+} from "./style"
+import { navLinks } from "@/constants/navlinks"
+
+export default function MenuHeader(){
+    const [menuIsOpen, setMenuIsOpen] = useState(false)
+    function handleMenuToggle(){
+        console.log(menuIsOpen)
+        setMenuIsOpen(!menuIsOpen)
+    }
+    return(
+        <MenuHeaderContainer>
+            <MenuToggle
+                type="checkbox"
+                id="menu-toggle-header"
+                checked={menuIsOpen}
+                onChange={handleMenuToggle}
+            />
+            <MenuToggleLabel
+                htmlFor="menu-toggle-header"
+            >
+                <MenuToggleLabelSpan
+                    menuIsOpen={menuIsOpen}
+                ></MenuToggleLabelSpan>
+            </MenuToggleLabel>
+            
+            <MenuBox menuIsOpen={menuIsOpen}>
+                {
+                    navLinks.map((item,key)=>(
+                        <li>
+                            <MenuBoxItem key={key} href={item.link}>{item.title}</MenuBoxItem>
+                        </li>
+                    ))
+                }
+            </MenuBox>
+        </MenuHeaderContainer>
+    )
+}
