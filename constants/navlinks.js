9739347d6d644a4a7e43fed43a88ccffaf39be1d
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-06-02 16:25:58 -0300

    Create navLinks file

---
+++ constants/navlinks.js
@@ -0,0 +1,8 @@
+export const navLinks = [
+    {link:"intro", title:"Intro"},
+    {link:"sobre-mim", title:"Sobre Mim"},
+    {link:"tech", title:"Tecnologias"},
+    {link:"projetos", title:"Projetos"},
+    {link:"certificados", title:"Certificados"},
+    {link:"contato", title:"Entre em Contato"}
+]
