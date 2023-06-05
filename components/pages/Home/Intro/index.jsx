86e8b376b4bcd77827ee75109473783fac97412c
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-06-05 18:55:00 -0300

    Create IntroSection Component

---
+++ components/pages/Home/Intro/index.jsx
@@ -0,0 +1,17 @@
+import EstruturaSection from "@/components/EstruturaSection";
+
+export default function IntroSection(){
+    return(
+        <EstruturaSection
+            idSection={"intro-section"}
+            idChangeSection={"about-me-section"}
+        >
+            <div>
+                Pedro Ceglia blablabla...
+            </div>
+            <div>
+                imagem blabla
+            </div>
+        </EstruturaSection>
+    )
+}
