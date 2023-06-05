2711f93d93f0f5e2c5f9759c9faa6c7ed92d66a1
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-06-05 18:55:36 -0300

    Create StacksSection Component

---
+++ components/pages/Home/Stacks/index.jsx
@@ -0,0 +1,17 @@
+import EstruturaSection from "@/components/EstruturaSection";
+
+export default function StacksSection(){
+    return(
+        <EstruturaSection
+            idSection={"stacks-section"}
+            idChangeSection={"intro-section"}
+        >
+            <div>
+                Stacks
+            </div>
+            <div>
+                Stacks Section
+            </div>
+        </EstruturaSection>
+    )
+}
