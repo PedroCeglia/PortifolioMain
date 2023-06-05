b9e197e7d692c692e73d216dac0810ac3a696f3f
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-06-05 18:55:45 -0300

    Create WhoImSection Component

---
+++ components/pages/Home/WhoIm/index.jsx
@@ -0,0 +1,17 @@
+import EstruturaSection from "@/components/EstruturaSection";
+
+export default function WhoImSection(){
+    return(
+        <EstruturaSection
+            idSection={"about-me-section"}
+            idChangeSection={"projetos-section"}
+        >
+            <div>
+                WhoIm
+            </div>
+            <div>
+                WhoIm Section
+            </div>
+        </EstruturaSection>
+    )
+}
