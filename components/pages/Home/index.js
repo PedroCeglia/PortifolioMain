b7caf4749f3f5638d5084d215a27a9d538bc5e73
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-06-05 18:54:37 -0300

    create home index.js

---
+++ components/pages/Home/index.js
@@ -0,0 +1,6 @@
+import IntroSection from "./Intro";
+import WhoImSection from "./WhoIm";
+import ProjetosSection from "./Projetos";
+import StacksSection from "./Stacks";
+
+export {IntroSection, WhoImSection, ProjetosSection, StacksSection}
