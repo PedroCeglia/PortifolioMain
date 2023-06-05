2dbf0a32cf28aaf1f477598ec6f1e085a9a3366b
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-06-05 18:55:08 -0300

    Create ProjetosSection Component

---
+++ components/pages/Home/Projetos/index.jsx
@@ -0,0 +1,17 @@
+import EstruturaSection from "@/components/EstruturaSection";
+
+export default function ProjetosSection(){
+    return(
+        <EstruturaSection
+            idSection={"projetos-section"}
+            idChangeSection={"stacks-section"}
+        >
+            <div>
+                Projetos
+            </div>
+            <div>
+                Projetos Section
+            </div>
+        </EstruturaSection>
+    )
+}
