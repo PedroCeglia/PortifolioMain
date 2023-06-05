5496980340c130873853cd6e0f34c5bee3601bf3
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-06-05 18:51:10 -0300

    Create Footer Component

---
+++ components/EstruturaPagina/Footer/style.js
@@ -0,0 +1,15 @@
+'use client'
+import styled from "styled-components";
+import style from "@/styles";
+
+export const FooterStyledComponent = styled.footer`
+    width: 100vw;
+    height: 60px;
+
+    background-color: red;
+    color: ${style.theme.color.c1};
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
