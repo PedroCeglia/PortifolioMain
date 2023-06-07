59cc12b5396c3e411417cf4f024964e1ceee5530
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-06-07 07:54:52 -0300

    Change "min-height" in "EstruturaSectionStyledComponent"

--- components/EstruturaSection/style.js
+++ components/EstruturaSection/style.js
@@ -5,13 +5,7 @@ import style from "@/styles"
 
 export const EstruturaSectionStyledComponent = styled.div`
     width: 100vw;
-    min-height: ${
-        props => props.isIntro?(
-            "calc(100vh - 125px)"
-        ):(
-            "calc(100vh - 25px)"
-        )
-    };
+    min-height: calc(100vh - 125px);
 
     position: relative;
 
