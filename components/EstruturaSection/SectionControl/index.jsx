cc963252dee1239cf3c1968d6c2a3b5627ee2e02
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-06-07 07:57:33 -0300

    change "motion.section" style and "div id={idSection}" style

--- components/EstruturaSection/SectionControl/index.jsx
+++ components/EstruturaSection/SectionControl/index.jsx
@@ -12,10 +12,12 @@ export default function SectionControl({idSection, children}){
             style={{
                 width:"100vw", height:"100%",
                 display:"flex", flexDirection:"column",
-                justifyContent:"flex-start", alignItems:"center"
+                justifyContent:"flex-start", alignItems:"center",
+                position:"relative"
+                
             }}
         >
-            <div id={idSection}></div>
+            <div id={idSection} style={{position:"absolute", top:"-100px"}}></div>
             {children}
         </motion.section>
     )
