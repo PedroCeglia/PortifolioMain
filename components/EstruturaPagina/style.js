be9273c8c05c6462772c8468528e45d66f2e1ed7
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-06-07 07:33:31 -0300

    add "overflow-y" in ContentEstruturaPaginaStyled

--- components/EstruturaPagina/style.js
+++ components/EstruturaPagina/style.js
@@ -14,5 +14,6 @@ export const EstruturaPaginaStyledComponent = styled.body`
 
 export const ContentEstruturaPaginaStyled = styled.div`
     min-height: calc(100vh - 160px);
-    width: 100%;
+    overflow-y: scroll;
+
 `
