3ebc3d3feea6cfa1b1c4f57bf5e63bbf0164ce2b
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-06-14 17:08:44 -0300

    Create IntroSection component

--- components/pages/Home/Intro/index.jsx
+++ components/pages/Home/Intro/index.jsx
@@ -1,17 +1,23 @@
 import EstruturaSection from "@/components/EstruturaSection";
+import InfoContainer from "./InfoContainer";
+import CarroselStacks from "./CarroselStacks";
+
+import { 
+    IntroSectionStyledComponent
+ } from "./style";
+
 
 export default function IntroSection(){
+
     return(
         <EstruturaSection
             idSection={"intro-section"}
             idChangeSection={"about-me-section"}
         >
-            <div>
-                Pedro Ceglia blablabla...
-            </div>
-            <div>
-                imagem blabla
-            </div>
+            <IntroSectionStyledComponent>
+                <InfoContainer/>
+                <CarroselStacks/>
+            </IntroSectionStyledComponent>
         </EstruturaSection>
     )
 }
