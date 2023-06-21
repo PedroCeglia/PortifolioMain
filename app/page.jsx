bf09bc3ea7d8d3d7fc1e7b9cfcf0bc18dc06aa3a
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-06-21 17:01:32 -0300

    Set "StacksSection" to "CertificadosSection"

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import {
     IntroSection, 
     WhoImSection, 
     ProjetosSection, 
-    StacksSection
+    CertificadosSection
 } from "@/components/pages/Home"
 
 export default function Home() {
@@ -11,7 +11,7 @@ export default function Home() {
       <IntroSection />
       <WhoImSection />
       <ProjetosSection />
-      <StacksSection />
+      <CertificadosSection />
     </>
   )
 }
