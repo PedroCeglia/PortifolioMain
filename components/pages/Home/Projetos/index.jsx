262a8ddf06e8c1558c013eaaf7c428a0936e96b8
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-06-21 17:14:27 -0300

    Import and use "ProjetosContainerStyledComponent" "ProjetosListStyledComponent", "ItemProjetos" and create a stactic project "projetoTeste" and a static project list

--- components/pages/Home/Projetos/index.jsx
+++ components/pages/Home/Projetos/index.jsx
@@ -1,17 +1,37 @@
 import EstruturaSection from "@/components/EstruturaSection";
+import { 
+    ProjetosContainerStyledComponent,
+    ProjetosListStyledComponent
+} from "./style";
+import ItemProjetos from "./ItemProjetos";
 
 export default function ProjetosSection(){
+
+    const projetoTeste = {
+        name:"Projeto Teste Teste",
+        image:"/assets/images/projects/forecast-weather/main.png",
+        link:"",
+        github:"",
+        descricao:""
+    }
+
+    const projetosList = [
+        projetoTeste,
+        projetoTeste,
+        projetoTeste,
+    ]
+
     return(
         <EstruturaSection
             idSection={"projetos-section"}
-            idChangeSection={"stacks-section"}
+            idChangeSection={"certificados-section"}
         >
-            <div>
-                Projetos
-            </div>
-            <div>
-                Projetos Section
-            </div>
+            <ProjetosContainerStyledComponent>
+                <h2>Projetos</h2>
+                <ProjetosListStyledComponent>
+                    { projetosList.map((projeto, key)=><ItemProjetos key={key} projeto={projeto}/>) }
+                </ProjetosListStyledComponent>
+            </ProjetosContainerStyledComponent>
         </EstruturaSection>
     )
 }
