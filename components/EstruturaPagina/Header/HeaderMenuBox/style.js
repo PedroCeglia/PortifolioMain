b4d92ef54f720116cbf7b61fab91d0ee0ce4231f
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-06-22 16:12:30 -0300

    Create "HeaderLinkScroll" Styled Component

--- components/EstruturaPagina/Header/HeaderMenuBox/style.js
+++ components/EstruturaPagina/Header/HeaderMenuBox/style.js
@@ -2,6 +2,7 @@
 
 import style from "@/styles"
 import styled from "styled-components"
+import { Link as LinkScroll} from "react-scroll"
 
 export const MenuBox = styled.ul`
     z-index: -1;
@@ -21,21 +22,26 @@ export const MenuBox = styled.ul`
     border-bottom-right-radius: 10px;
     border-bottom-left-radius: 10px;
 
+
     ${({ menuIsOpen }) => menuIsOpen && `
         top: 100px !important;
     `}
 `
-export const MenuBoxItem = styled.a`
-  display: block;
-  padding: 12px 24px;
-  color: ${style.theme.color.c2};
-  font-family: 'Roboto', sans-serif;
-  font-size: 20px;
-  font-weight: 600;
-  text-decoration: none;
-  transition-duration: 0.25s;
 
-  &:hover {
-    background-color: #cfd8dc;
-  }
+
+export const HeaderLinkScroll = styled(LinkScroll)`
+    display: block;
+    padding: 12px 24px;
+    color: ${style.theme.color.c2};
+    font-family: 'Roboto', sans-serif;
+    font-size: 20px;
+    font-weight: 600;
+    text-decoration: none;
+    transition-duration: 0.25s;
+    cursor: pointer;
+    
+
+    &:hover {
+      background-color: #cfd8dc;
+    } 
 `
