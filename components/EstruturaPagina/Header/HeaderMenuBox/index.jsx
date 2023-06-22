c5991812c2a43e97594219686b9683b0cd0e2d18
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-06-22 16:12:04 -0300

    create "handleMenuToggle" props and use in "HeaderLinkScroll onClink"

--- components/EstruturaPagina/Header/HeaderMenuBox/index.jsx
+++ components/EstruturaPagina/Header/HeaderMenuBox/index.jsx
@@ -1,13 +1,20 @@
-import { MenuBox, MenuBoxItem } from "./style"
+import { MenuBox, HeaderLinkScroll } from "./style"
 import { navLinks } from "@/constants/navlinks"
 
-export default function HeaderMenuBox({menuIsOpen}){
+export default function HeaderMenuBox({menuIsOpen, handleMenuToggle}){
     return(               
         <MenuBox menuIsOpen={menuIsOpen}>
             {
-                navLinks.map((item,key)=>(
-                    <li key={key}>
-                        <MenuBoxItem href={item.link}> {item.title} </MenuBoxItem>
+                navLinks.map(( item, key ) => (
+                    <li key={ key } >
+                        <HeaderLinkScroll
+                            to={ item.link }
+                            smooth
+                            onClick={handleMenuToggle}
+                            duration={ 2000 }
+                        > 
+                            {item.title} 
+                        </HeaderLinkScroll>
                     </li>
                 ))
             }
