4b1299f5302b1238d16020fc9ec3ff00eee1f69f
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-06-22 12:01:03 -0300

    Create "filterListBySizeAndPosition,  filterListByPosition & filterListBySize" private functions

--- firebase/Database/index.js
+++ firebase/Database/index.js
@@ -1,6 +1,11 @@
 import { database } from "..";
 import { ref, onValue } from "firebase/database";
 
+const getProjectsRef = () => ref(database, "projetos/intro")
+const getCertificadosRef = () => ref( database, "certificados/intro" )
+const getProjectRef = ( id ) => ref( database, `projetos/descricao/${id}` )
+const getCertificadoRef = ( id ) => ref( database, `certificados/descricao/${ id }` )  
+
 export function getAllProjectList( setProjectList, max6=false ) {
     onValue( getProjectsRef(), snapshot => {
         let projectList = []
@@ -8,45 +13,40 @@ export function getAllProjectList( setProjectList, max6=false ) {
             projectList.push( project.val() )
         })
 
-        const filterProjectListByPosition = projectList.filter( ( a ,b ) => a.posicao - b.posicao )
-        const filterProjectListBySize = () => ( max6 &&  filterProjectListByPosition.length > 5) ? (
-            filterProjectListByPosition.slice( 0, 5 ) ) : filterProjectListByPosition
-        
-
-        const projectListFinal =  filterProjectListBySize()
+        const projectListFinal =  filterListBySizeAndPosition(max6, projectList)
         setProjectList( projectListFinal )
     })
 }
-const getProjectsRef = () => ref(database, "projetos/intro")
-
-export function getProjectById( setProject, id ) {
-    onValue( getProjectRef( id ), project => {
-        setProject( project.val() )
-    })
-}
-const getProjectRef = ( id ) => ref( database, `projetos/descricao/${id}` )
-
-
 export function getAllCertificadoList( setCertificadosList, max6=false ) {
     onValue( getCertificadosRef(), snapshot => {
         let certificadosList = []
         snapshot.forEach( certificado => {
             certificadosList.push( certificado.val() )
         })
-
-        const filterCertificadosListByPosition = certificadosList.filter( ( a, b ) => a.position - b.position )
-        const filterCertificadosListBySize = () => ( max6 && filterCertificadosListByPosition.length > 5) ? (
-                filterCertificadosListByPosition.slice( 0, 5 ) ) : filterCertificadosListByPosition
-
-        const certificadosListFinal = filterCertificadosListBySize()
+        const certificadosListFinal = filterListBySizeAndPosition(max6, certificadosList)
         setCertificadosList( certificadosListFinal )
     })
 }
-const getCertificadosRef = () => ref( database, "certificados/intro" )
+
+function filterListBySizeAndPosition( max6, editList ){
+    filterListByPosition(editList)
+    filterListBySize(editList, max6)
+    return editList 
+}
+const filterListByPosition = ( filterList ) => filterList.sort( ( a ,b ) => a.posicao - b.posicao )
+const filterListBySize = ( filterList, max6 ) => {
+    filterList = ( max6 &&  filterList.length > 5) ? filterList.slice( 0, 6 ) : filterList
+}
+
+
+
+
 
 export function getCertificadoById( setCertificado, id ){
     onValue( getCertificadoRef( id ), certificado => {
         setCertificado( certificado )
     })
 }
-const getCertificadoRef = ( id ) => ref( database, `certificados/descricao/${ id }` )
+      
+
+
