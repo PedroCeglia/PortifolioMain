cc9f646751c337e84cfa486a1dfc042777883ea1
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-06-27 14:59:15 -0300

    clean css code

--- components/Itens/ItemCertificado/style.js
+++ components/Itens/ItemCertificado/style.js
@@ -7,9 +7,6 @@ export const ItemCertificadoStyledComponent = styled.div`
     width: 25vw;
     min-width: 280px;
     margin: 20px 1%;
-
-    border-radius: 20px;
-
     padding-bottom: 20px;
 
     display: flex;
@@ -17,50 +14,60 @@ export const ItemCertificadoStyledComponent = styled.div`
     justify-content: flex-start;
     align-items: flex-start;
 
-    background-color: ${style.theme.rgbaFunctions.getDarkColorRGBA(.8)};
-    color: ${style.theme.color.c1};
-    font-family: ${style.fonts.fontsFamily.ubunto};
+    border-radius: 20px;
+    background-color: ${ style.theme.rgbaFunctions.getDarkColorRGBA(.8) };
+    color: ${ style.theme.color.c1 };
+    font-family: ${ style.fonts.fontsFamily.ubunto };
 
-    img{
+    img {
         width: 95%;
         height: 80%;
-        align-self: center;
         object-fit: cover;
+
+        align-self: center;
         margin: 10px 0;
         border-radius: 10px;
     } 
 
-    h3{
+    h3 {
         margin: 7px 15px;
-        font-size: ${style.fonts.fontsSize.littleBig};
+        font-size: ${ style.fonts.fontsSize.littleBig };
     }
-    span{
+
+    span {
         margin: 0 15px 7px;
     }
-    div{
+
+    div {
         margin-top: 20px;
         width: 100%;
+
         display: flex;
         justify-content: space-around;
         align-items: center;
     }
-    div a{
+
+    div a {
         width: 40%;
-        text-decoration: none;
-        color: ${style.theme.color.c2};
-        text-align: center;
         padding: 5px 10px;
+
         border-radius: 5px;
-        background-color: ${style.theme.rgbaFunctions.getLightColorRGBA(.6)};
-    }
-    div a:hover{
-        background-color: ${style.theme.rgbaFunctions.getLightColorRGBA(1)};
+        background-color: ${ style.theme.rgbaFunctions.getLightColorRGBA(.6) };
+        color: ${ style.theme.color.c2 };
+
+        text-decoration: none;
+        text-align: center;
+        cursor: pointer;    
+        
+        &:hover {
+            background-color: ${ style.theme.rgbaFunctions.getLightColorRGBA(1) };
+        }
     }
 
-    @media all and (max-width:900px){
+    @media all and ( max-width:900px ) {
         width: 45%;
     }
-    @media all and (max-width:620px){
+    @media all and ( max-width:620px ) {
         width: 80%;
     }
 `
