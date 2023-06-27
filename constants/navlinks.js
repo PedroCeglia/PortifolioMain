5a46a19e9ab3106a6105016c6b9ce72f7f5616b7
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-06-27 15:05:13 -0300

    set "navLinks" name to "navLinksSections" and create an other list "navLinksPage"

--- constants/navlinks.js
+++ constants/navlinks.js
@@ -1,7 +1,13 @@
-export const navLinks = [
+export const navLinksSections = [
     {link:"intro-section", title:"Intro"},
     {link:"about-me-section", title:"Sobre Mim"},
     {link:"projetos-section", title:"Projetos"},
     {link:"certificados-section", title:"Certificados"},
     {link:"contato", title:"Entre em Contato"}
+]
+
+export const navLinksPage = [
+    {link:"/#top-page", title:"Home"},
+    {link:"/certificados#top-page", title:"Certificados"},
+    {link:"/projetos#top-page", title:"Projetos"}
 ]
