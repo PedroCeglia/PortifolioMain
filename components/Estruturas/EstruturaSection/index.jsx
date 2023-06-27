ae8d1b41b68d0f7bd71715b366a64a599a51265a
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-06-27 14:57:30 -0300

    set "verifyIsOdd" function and verify if "idChangeSection" is null, if it isnt, return "ChangeSection" component

--- components/Estruturas/EstruturaSection/index.jsx
+++ components/Estruturas/EstruturaSection/index.jsx
@@ -2,23 +2,24 @@ import SectionControl from "./SectionControl"
 import ChangeSection from "./ChangeSection"
 import { EstruturaSectionStyledComponent } from "./style"
 
+export default function EstruturaSection( { children, idSection, idChangeSection = null } ) {
 
-export default function EstruturaSection({children, idSection, idChangeSection}){
-
-    const verifyIsOdd = () => (idSection=="intro-section"||idSection=="projetos-section")
-    const verifyIsIntro = () => (idSection=="intro-section")
+    const verifyIsOdd = () => ( 
+        idSection == "intro-section" ||
+        idSection == "projetos-section" ||
+        idSection == "intro-certificado-section"
+    )
     const isOdd = verifyIsOdd()
+
+    const verifyIsIntro = () => ( idSection == "intro-section" )
     const isIntro = verifyIsIntro()
  
     return(
-        
-            <SectionControl idSection={idSection}>
-                <EstruturaSectionStyledComponent isOdd={isOdd} isIntro={isIntro}>
-                    {children}
-                    <ChangeSection slug={idChangeSection}/>
+            <SectionControl idSection={ idSection } >
+                <EstruturaSectionStyledComponent isOdd={ isOdd } isIntro={ isIntro } >
+                    { children }
+                    { idChangeSection != null && <ChangeSection slug={ idChangeSection } /> }
                 </EstruturaSectionStyledComponent> 
-            </SectionControl>
-            
-                   
+            </SectionControl>           
     )
 }
