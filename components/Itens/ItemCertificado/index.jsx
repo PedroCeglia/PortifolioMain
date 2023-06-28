17615a45b05e8a69c81d8aa1ebadc77137c9cb4d
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-06-28 18:37:53 -0300

    Add span "hora" em "ItemCertificado"

--- components/Itens/ItemCertificado/index.jsx
+++ components/Itens/ItemCertificado/index.jsx
@@ -7,8 +7,11 @@ export default function ItemCertificado( { item } ) {
         <ItemCertificadoStyledComponent>
             <EstruturaImagem src={ item.image } alt={ item.name } title={ item.name } />
             <h3> { item.name} </h3>
-            <span> { item.instituicao } </span>
-            <span> { item.data } </span>
+            <span> { item.instituicao }  </span>
+            <p> 
+                <span> { item.data } </span> 
+                <span> { item.horas } Horas </span> 
+            </p>
             <div>
                 <a href={ item.link } target={ "_blanc" } > Abrir Certificado </a>
                 <Link href={ `/certificados/${ item.id }#page-top` }> Descrição </Link>
