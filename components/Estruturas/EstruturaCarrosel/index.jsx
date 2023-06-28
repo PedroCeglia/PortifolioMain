c658bd8a535d6698b616e9b663964c7954a84588
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-06-28 18:36:38 -0300

    if the "title" isnt null, return "<h2> { title } </h2>"

--- components/Estruturas/EstruturaCarrosel/index.jsx
+++ components/Estruturas/EstruturaCarrosel/index.jsx
@@ -1,10 +1,10 @@
 import CarroselContent from "./CarrosselContent"
 import { EstruturaCarrosselStyledComponent } from "./style"
 
-export default function EstruturaCarrossel( { title, type } ) {
+export default function EstruturaCarrossel( { title = null, type } ) {
     return (
         <EstruturaCarrosselStyledComponent>
-            <h2>{title}</h2>
+            { title != null && <h2> { title } </h2> }
             <CarroselContent type={type}/>
         </EstruturaCarrosselStyledComponent>
     )
