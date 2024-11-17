7dbfb032847f9f77f453579c350e0cb4aa8524b5
PedroCeglia <dev.ceglia.pedro@gmail.com>
2024-11-17 00:33:45 -0300

    .env

--- firebase/index.js
+++ firebase/index.js
@@ -8,7 +8,7 @@ import { getDatabase } from "firebase/database"
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-export const firebaseConfig = {
+export const firebaseConfig2 = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY_FIREBASE,
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
   databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
@@ -17,6 +17,17 @@ export const firebaseConfig = {
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
+};
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyC4VwDergW6pHpGqbbK_H7Sk-8k8zEYVj0",
+  authDomain: "portifolio-16fa8.firebaseapp.com",
+  databaseURL: "https://portifolio-16fa8-default-rtdb.firebaseio.com",
+  projectId: "portifolio-16fa8",
+  storageBucket: "portifolio-16fa8.appspot.com",
+  messagingSenderId: "428844026206",
+  appId: "1:428844026206:web:a8d6cbe0c82023999e27cf",
+  measurementId: "G-F1M0TRBYHV"
 };
 
 // Initialize Firebase
